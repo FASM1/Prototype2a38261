0caafbdcefc238b09dc3e88d85984cf1b5568d8c
FASM1 <filipeari@live.com.pt>
2024-05-13 12:29:01 +0100

    3.Test for spacebar press
    a38261@3.Test for spacebar press

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,6 +23,9 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange){
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //Launch a projectile from the player
+        }
      //horizontalInput = Input.GetAxis("Horizontal");
      //transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
